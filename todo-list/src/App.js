997e50b8b0554b889ad7d4c7db4d3fa99757e8b2
kimkyunghoon <byul2sol@naver.com>
2019-12-18 16:46:13 +0900

    todo-list project와 .gitignore 추가

---
+++ todo-list/src/App.js
@@ -0,0 +1,65 @@
+import React, { useState, useRef, useCallback } from 'react';
+import TodoTemplate from './components/TodoTemplate';
+import TodoInsert from './components/TodoInsert';
+import TodoList from './components/TodoList';
+
+const App = (props) => {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: '리액트',
+      checked: true,
+    },
+    {
+      id: 2,
+      text: 'vue',
+      checked: true,
+    },
+    {
+      id: 3,
+      text: 'react',
+      checked: false,
+    }
+  ])
+  const nextId = useRef(4);
+
+  const onInsert = useCallback(
+    text=> {
+      const todo = {
+        id: nextId.current,
+        text,
+        checked: false,
+      };
+      setTodos(todos.concat(todo));
+      nextId.current += 1;
+    },
+    [todos],
+  )
+  
+  const onRemove = useCallback(
+    id => {
+      setTodos(todos.filter(todo => todo.id !== id));
+    },
+    [todos],
+  );
+
+  const onToggle = useCallback(
+    id => {
+      setTodos(
+        todos.map(todo =>
+          todo.id === id ? { ...todo, checked: !todo.checked } : todo,
+          ),
+      );
+    },
+    [todos],
+  )
+
+  return (
+    <TodoTemplate>
+      <TodoInsert onInsert={onInsert}/>
+      <TodoList todos={todos} onRemove={onRemove} onToggle={onToggle}/>
+    </TodoTemplate>
+  );
+};
+
+export default App;
