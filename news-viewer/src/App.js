6be8ded21c59e9a28424d485101a85043404a208
kimkyunghoon <byul2sol@naver.com>
2020-01-30 21:46:47 +0900

    NewsApi 받아오는 코드 추가

--- news-viewer/src/App.js
+++ news-viewer/src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios';
 
-function App() {
+const App = (props) => {
+  const [data, setData] = useState(null);
+  const onClick = () => {
+    axios.get('https://newsapi.org/v2/top-headlines?country=kr&apiKey=4bfb8b1da90a497b97f5ee81cb843c37').then(resposne => {
+      setData(resposne.data);
+    });
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <button onClick={onClick}>불러오기</button>
+      </div>
+      {data && <textarea rows={7} value={JSON.stringify(data, null, 2)} readOnly={true}/>}
     </div>
   );
-}
+};
 
 export default App;
