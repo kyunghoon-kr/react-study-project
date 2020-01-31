d8b7fb0d0eedc11db420090cd9f4d5c443ac4af3
kimkyunghoon <byul2sol@naver.com>
2020-01-31 13:39:20 +0900

    NewsList.js 추가

--- news-viewer/src/App.js
+++ news-viewer/src/App.js
@@ -1,20 +1,16 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import NewsList from './components/NewsList';
 
 const App = (props) => {
-  const [data, setData] = useState(null);
-  const onClick = () => {
-    axios.get('https://newsapi.org/v2/top-headlines?country=kr&apiKey=4bfb8b1da90a497b97f5ee81cb843c37').then(resposne => {
-      setData(resposne.data);
-    });
-  };
+  // const [data, setData] = useState(null);
+  // const onClick = () => {
+  //   axios.get('https://newsapi.org/v2/top-headlines?country=kr&apiKey=4bfb8b1da90a497b97f5ee81cb843c37').then(resposne => {
+  //     setData(resposne.data);
+  //   });
+  // };
   return (
-    <div>
-      <div>
-        <button onClick={onClick}>불러오기</button>
-      </div>
-      {data && <textarea rows={7} value={JSON.stringify(data, null, 2)} readOnly={true}/>}
-    </div>
+    <NewsList/>
   );
 };
 
