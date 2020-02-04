863c4dc98aa06fd6be2f5c23df622027184e797c
kimkyunghoon <byul2sol@naver.com>
2020-02-04 12:51:41 +0900

    index.js에 useTodoState 재적용

--- react-hooks-todo/src/index.js
+++ react-hooks-todo/src/index.js
@@ -1,13 +1,14 @@
-import React, { useState } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import Typography from '@material-ui/core/Typography';
-import './styles.css';
 import TodoForm from './TodoForm';
 import TodoList from './TodoList';
 import useTodoState from './useTodoState';
+import './styles.css';
 
 const App = () => {
-  const [todos, addTodo, deleteTodo] = useTodoState([]);
+  const { todos, addTodo, deleteTodo } = useTodoState([]);
+
   return (
     <div className="App">
       <Typography component="h1" variant="h2">
@@ -19,19 +20,13 @@ const App = () => {
           const trimmedText = todoText.trim();
 
           if (trimmedText.length > 0) {
-            setTodos([...todos, trimmedText]);
+            addTodo(trimmedText);
           }
         }}
       />
-      <TodoList 
-        todos={todos}
-        deleteTodo={(todoIndex) => {
-        const newTodos = todos.filter((_, index) => index !== todoIndex);
-        setTodos(newTodos);
-      }}/>
-    </div>
 
-    
+      <TodoList todos={todos} deleteTodo={deleteTodo} />
+    </div>
   );
 };
 
