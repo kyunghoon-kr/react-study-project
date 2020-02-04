b4c2ec02c59ec98be473957f672f251eaf1d47e3
kimkyunghoon <byul2sol@naver.com>
2020-02-04 12:50:11 +0900

    useInputState.js 추가 - TodoForm에 적용

--- react-hooks-todo/src/TodoForm.js
+++ react-hooks-todo/src/TodoForm.js
@@ -1,27 +1,28 @@
-import React, { useState } from 'react';
+import React from 'react';
 import TextField from '@material-ui/core/TextField';
+import useInputState from './useInputState';
 
 const TodoForm = ({ saveTodo }) => {
-    const [value, setValue] = useState('');
-    return (
-        <form
-        onSubmit={(event) => {
+  const { value, reset, onChange } = useInputState('');
+
+  return (
+    <form
+      onSubmit={(event) => {
         event.preventDefault();
+
         saveTodo(value);
-        setValue('');
+        reset();
       }}
-       >
+    >
       <TextField
         variant="outlined"
         placeholder="Add todo"
         margin="normal"
-        onChange={(event) => {
-          setValue(event.target.value);
-        }}
+        onChange={onChange}
         value={value}
       />
     </form>
-    );
+  );
 };
 
 export default TodoForm;
