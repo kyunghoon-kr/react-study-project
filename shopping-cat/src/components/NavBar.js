e6f6aa7d6b0337b7085f3ec43707d7a09526cb0e
kimkyunghoon <byul2sol@naver.com>
2020-02-14 22:03:00 +0900

    기본 컴포넌트 구성

---
+++ shopping-cat/src/components/NavBar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NavBar = (props) => {
+    return (
+        <div>
+            <button>제품목록</button>
+            <button>장바구니</button>
+        </div>
+    );
+};
+
+export default NavBar;
