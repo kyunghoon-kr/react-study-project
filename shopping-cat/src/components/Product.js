a00f8878a983a69f14f9ee259ece2a51e28732f8
kimkyunghoon <byul2sol@naver.com>
2020-02-18 14:17:26 +0900

    반응형 웹 디자인 추가 1. 미디어 쿼리를 이용하여 모바일/태블릿/pc 대응 2. 각 기기별로 엔드포인트를 두어 ui 변화

--- shopping-cat/src/components/Product.js
+++ shopping-cat/src/components/Product.js
@@ -1,37 +1,71 @@
 import React from 'react';
 import styled from 'styled-components';
 
+
+// Breakpoing pc: 1024, tablet: 768, mobile: 320
+
 const ProductTemplate = styled.div`
-    width: 200px;
     display: flex;
     flex-direction: column;
     border: 2px solid;
     margin-top: 10px;
+
+    @media only screen and (min-width: 320px) {
+        width: 45%;
+        margin-left: 10px;
+        margin-bottom: 30px;
+        button {
+            font-size: 1.0rem;
+        }
+        p {
+            font-size: 0.7rem;
+        }
+        img {
+            height: 150px;
+        }
+    }
+    @media only screen and (min-width: 768px) {
+        width: 25%;
+        margin-left: 30px;
+        p {
+            font-size: 1.0rem;
+        }
+        button {
+            font-size: 1.4rem;
+        }
+        
+    }
+    @media only screen and (min-width: 1024px) {
+        width: 23%;
+        margin-left: 10px;
+        margin-bottom: 5px;
+        p {
+            font-size: 1.1rem;
+        }
+        button {
+            font-size: 1.6rem;
+        }
+        img {
+            height: 200px;
+        }
+    }
+
     &:hover {
         border: 3px solid;
         border-color: red;
-
-    }
-    img {
-        width: 200px;
-        height: 100px;
     }
     button {
         border: 1px solid;
         border-color: aquamarine;
         color: aquamarine;
-        font-size: 1.4rem;
         &:hover {
             font-weight: bold;
             color: red;
             border: 0px;
         }
     }
-    p {
-        font-size: 1rem;
-    }
-
 `;
+
 
 
 const Product = ({product, onSelect, isBasket, onAdd, onSub}) => {
--- shopping-cat/src/components/ProductList.js
+++ shopping-cat/src/components/ProductList.js
@@ -2,12 +2,24 @@ import React from 'react';
 import Product from './Product';
 import styled from 'styled-components';
 
+// Breakpoint pc: 1024, tablet: 768, mobile: 320
+
 const ProductListTemplate = styled.div
 `
-    padding: 50px;
     display: flex;
     align-items: flex-start;
     flex-wrap: wrap;
+
+    @media only screen and (min-width: 320px) {
+        padding: 10px;
+    }
+    @media only screen and (min-width: 768px) {
+        padding: 30px;
+    }
+    @media only screen and (min-width: 1024px) {
+        padding: 10px;
+    }
+
     button {
         align-items: flex-end;
         justify-content: center;
@@ -20,7 +32,7 @@ const ProductList = ({products, onSelect, isBasket, onAdd, onSub, onSubmit}) => 
             {products.map(product => (
                 <Product product={product} key={product.id} onSelect={onSelect} isBasket={isBasket} onAdd={onAdd} onSub={onSub}/>
             ))}
-            {onSubmit ? <button onClick={onSubmit}>제출하기d</button> : <></>}
+            {onSubmit ? <button onClick={onSubmit}>제출하기</button> : <></>}
         </ProductListTemplate>
     );
 };
